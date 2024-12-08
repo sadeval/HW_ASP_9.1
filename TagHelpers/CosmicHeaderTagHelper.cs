c0a708df01a6f32262b377cd5c615cbedcb3786c
sadeval <sadeval2011@gmail.com>
2024-12-08 18:23:11 +0200

    Add project files.

---
+++ TagHelpers/CosmicHeaderTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HW_ASP_9._1.TagHelpers
+{
+    [HtmlTargetElement("cosmic-header")]
+    public class CosmicHeaderTagHelper : TagHelper
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h1";
+            output.Attributes.Add("class", "cosmic-header");
+            output.Content.SetHtmlContent($@"
+                <span class='cosmic-text'>{Title}</span>
+            ");
+        }
+    }
+}
